621c59a6fcfb8887bc7ac59a4a7fc431326f34d2
ilsimi1066 <diegosimi2k5@gmail.com>
2025-09-19 14:50:57 +0200

    Added first things and scripts

---
+++ OZ/Assets/Scripts/Target.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Vector2 followSpot;
+    public float speed;
+    public float perspectiveScale;
+    public float scaleRatio;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        followSpot = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0))
+        {
+            followSpot = new Vector2(mousePosition.x, mousePosition.y);
+        }
+        transform.position = Vector2.MoveTowards(transform.position, followSpot, Time.deltaTime * speed);
+        AdjustPerspective();
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        followSpot = transform.position;
+    }
+
+    private void AdjustPerspective()
+    {
+        Vector3 scale = transform.localScale;
+        scale.x = perspectiveScale * (scaleRatio - transform.position.y);
+        scale.y = perspectiveScale * (scaleRatio - transform.position.y);
+        transform.localScale = scale;
+    }
+}
